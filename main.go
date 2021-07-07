3de8755932871a9c0d8a0670e89ff928935a39a3
akshat-sahijpal <20akshatsahijpal01droid01@gmail.com>
2021-07-08 01:58:09 +0530

    v1

--- main.go
+++ main.go
@@ -2,56 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io"
+	"os"
 )
 
 func main() {
-	ch := make(chan string)
-	ch0 := make(chan string)
-	a := make(chan int, 100)
-	b := make(chan int, 100)
-	go worker(a, b)
-	for i := 0; i < 100; i++ {
-		a <- i
+	fileReader()
+}
+func fileReader() {
+	file, error := os.Open("run.txt") // Open Returns pointer to the file
+	if error != nil {
+		panic(error)
 	}
-	close(a)
-	for i := 0; i < 100; i++ {
-		fmt.Println(<-b)
-	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error Closing the file ", err)
+		}
+	}(file)
 
-	go func() {
-		for {
-			ch <- "A" // Sender
-			time.Sleep(time.Second * 2)
-		}
-	}()
-	go func() {
-		for {
-			ch0 <- "B" // Sender
-			time.Sleep(time.Millisecond * 5000)
-		}
-	}()
+	// Data is stored in a buffer
+	buffer := make([]byte, 10) // buffer of size 10
+	// func (f *File) Read(b []byte) (n int, err error)
 	for {
-		select { // Selects the channel thats available at that time
-		case <-ch:
-			fmt.Println(<-ch) // Receiver
-		case <-ch0:
-			fmt.Println(<-ch0) // Receiver
+		n, err := file.Read(buffer)
+		if err == io.EOF {
+			return
 		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(buffer[:n]))
 	}
-}
-func workerThread(
-	jobs <-chan int, //Only Receive
-	results chan<- int /*Only Send */) {
-}
-func worker(jobs <-chan int /*Receive*/, res chan<- int /*Send*/) {
-	for i := range jobs {
-		res <- calc(i)
-	}
-}
-func calc(i int) int {
-	if i == 1 {
-		return 1
-	}
-	return calc(i+1) + calc(i+2)
 }
