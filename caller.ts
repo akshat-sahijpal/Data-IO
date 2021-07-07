1a7338acceea7f903b8c8f6588175d009f023a03
akshat-sahijpal <20akshatsahijpal01droid01@gmail.com>
2021-07-07 17:28:47 +0530

    v1

---
+++ caller.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+function main (T : bigint) : string {
+    return "K"
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
-func main() {
-	fmt.Println("")
+type AlphaReader struct {
+	src io.Reader
 }
+
+func NewAlphaReader(src io.Reader) *AlphaReader {
+	return &AlphaReader{src: src}
+}
+func (reader *AlphaReader) Read(p *[]byte) (s0 int, err error) {
+	fmt.Println(*reader)
+	for i := 0; i < len(*p); i++ {
+		if ((*reader)[i] >= 'A' && (*reader)[i] <= 'Z') || ((*reader)[i] >= 'a' && (*reader)[i] <= 'z') {
+			(*p)[i] = (*reader)[i]
+			if (*p)[i+1] == 0 {
+				return 0, io.EOF
+			}
+		}
+		s0++
+	}
+
+	return s0, nil
+}
+
+func main() {}
