a7cdd55067a8acc9f02d07f1a99c05d59672a7ad
akshat-sahijpal <20akshatsahijpal01droid01@gmail.com>
2021-07-08 00:56:50 +0530

    v1

--- main.go
+++ main.go
@@ -2,22 +2,56 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
-	"strings"
+	"time"
 )
 
 func main() {
-	ReadD()
+	ch := make(chan string)
+	ch0 := make(chan string)
+	a := make(chan int, 100)
+	b := make(chan int, 100)
+	go worker(a, b)
+	for i := 0; i < 100; i++ {
+		a <- i
+	}
+	close(a)
+	for i := 0; i < 100; i++ {
+		fmt.Println(<-b)
+	}
+
+	go func() {
+		for {
+			ch <- "A" // Sender
+			time.Sleep(time.Second * 2)
+		}
+	}()
+	go func() {
+		for {
+			ch0 <- "B" // Sender
+			time.Sleep(time.Millisecond * 5000)
+		}
+	}()
+	for {
+		select { // Selects the channel thats available at that time
+		case <-ch:
+			fmt.Println(<-ch) // Receiver
+		case <-ch0:
+			fmt.Println(<-ch0) // Receiver
+		}
+	}
 }
-func ReadD() {
-	r := strings.NewReader("Reading....")
-	_, err := io.Copy(os.Stdout, r)
-	if err != nil {
-		fmt.Println("Error")
+func workerThread(
+	jobs <-chan int, //Only Receive
+	results chan<- int /*Only Send */) {
+}
+func worker(jobs <-chan int /*Receive*/, res chan<- int /*Send*/) {
+	for i := range jobs {
+		res <- calc(i)
 	}
-	_, err = io.CopyBuffer(os.Stdout, strings.NewReader("Akshat Sahijpal"), make([]byte, 8))
-	if err != nil {
-		return
+}
+func calc(i int) int {
+	if i == 1 {
+		return 1
 	}
+	return calc(i+1) + calc(i+2)
 }
