52ba89a3945d6ec261693109b5d731db138b4dde
akshat-sahijpal <20akshatsahijpal01droid01@gmail.com>
2021-07-10 03:20:07 +0530

    v1

--- main.go
+++ main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
 )
 
+type Data struct {
+	Name   string `json:"name"`
+	Age    int    `json:"age"`
+	Dob    int    `json:"dob"`
+	Linker Link   `json:"linker"`
+}
+
+type Link struct {
+	Linked string   `json:"-"`
+	Hash   HashedIn `json:"hash"`
+}
+type HashedIn struct {
+	Next *Data `json:"next"`
+}
+
 func main() {
-	file, err := os.OpenFile("run.txt", os.O_RDWR|os.O_TRUNC, 0600)
+	Json()
+}
+func Json() {
+	f := Data{
+		Name: "ME",
+		Age:  233,
+		Dob:  03,
+		Linker: Link{
+			Linked: "CA",
+		},
+	}
+
+	e := Data{
+		Name: "SE",
+		Age:  23,
+		Dob:  03,
+		Linker: Link{
+			Linked: "CA",
+			Hash: HashedIn{
+				Next: &f,
+			},
+		},
+	}
+	d := Data{
+		Name: "Akshat",
+		Age:  20,
+		Dob:  2001,
+		Linker: Link{
+			Linked: "JSON",
+			Hash: HashedIn{
+				Next: &e,
+			},
+		},
+	}
+	marshal, err := json.Marshal(d)
 	if err != nil {
-		fmt.Println("Error Loading File!!")
+		panic(err)
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Println("Error Closing the File!!")
-		}
-	}(file)
-	// Write the data
-	buffer := []byte("Buffered data 1")
-	_, err1 := file.Write(buffer)
-	if err1 != nil {
-		return
-	} else {
-		fmt.Println("Done!!!")
-	}
+	fmt.Println(string(marshal))
 }
